5d1348152735819f59b3b381df9bbe6c74a6a8ef
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-09-05 23:02:00 +0530

    updates

--- Other/Airbnb Clone/script.js
+++ Other/Airbnb Clone/script.js
@@ -1,6 +1,8 @@
 // https://www.figma.com/file/QOCWgizbc2sBwnC7onGAQO/Airbnb---Home%2C-Search%2C-and-Listing-Pages-(Community)?type=design&node-id=0-1&mode=design
 
 const searchByLocation = async () => {
+  // main: location, checkin, checkout, adults
+  // optional: children, infants, pets, page, currency
   const url =
     "https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD";
   const options = {
@@ -20,6 +22,8 @@ const searchByLocation = async () => {
   }
 };
 const calender = async () => {
+  // main: room_id
+  // optional: currency, year, month ,count
   const url =
     "https://airbnb13.p.rapidapi.com/calendar?room_id=18951965&currency=USD&year=2023&month=12&count=1";
   const options = {
@@ -40,6 +44,7 @@ const calender = async () => {
 };
 
 const autoComplete = async () => {
+  // main: query
   const url = "https://airbnb13.p.rapidapi.com/autocomplete?query=paris";
   const options = {
     method: "GET",
@@ -58,6 +63,8 @@ const autoComplete = async () => {
   }
 };
 const searchByGeoCoordinates = async () => {
+  // main: ne_lat, ne_lng, sw_lat, sw_lng, checkin, checkout, adults
+  // optional: children, infants, pets, page, currency
   const url =
     "https://airbnb13.p.rapidapi.com/search-geo?ne_lat=52.51&ne_lng=13.41&sw_lat=52.41&sw_lng=13.31&checkin=2023-09-15&checkout=2023-09-16&adults=1&children=0&infants=0&pets=0&page=1&currency=USD";
   const options = {
